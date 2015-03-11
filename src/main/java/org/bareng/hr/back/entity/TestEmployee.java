6d098b962acbd190fb4b902c721b79c5c8956acc
hansas <shandoko.1010@gmail.com>
2015-03-11 09:25:59 +0700

    test add file TestEmployee ke git
    Signed-off-by: hansas <shandoko.1010@gmail.com>

---
+++ src/main/java/org/bareng/hr/back/entity/TestEmployee.java
@@ -0,0 +1,61 @@
+package org.bareng.hr.back.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@SuppressWarnings("serial")
+public class TestEmployee implements Serializable {
+	@NotNull
+	private Integer id;
+	
+	@NotBlank
+	private String regNumber;
+	
+	@NotBlank
+	private String fullName;
+	
+	@NotNull
+	@DateTimeFormat(pattern="dd/MM/yyyy")
+	private Date birthDate;
+	
+	@NotBlank 
+	@Email
+	private String emailAddress;
+	
+	public Integer getId() {
+		return id;
+	}
+	void setId(Integer id) {
+		this.id = id;
+	}
+	public String getRegNumber() {
+		return regNumber;
+	}
+	public void setRegNumber(String regNumber) {
+		this.regNumber = regNumber;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+}
